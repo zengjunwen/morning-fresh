dd6538b39edb22d856ea22f6cd4c23d06ecc840a
junwen <1317786610@qq.com>
2020-01-22 00:22:45 +0800

    开发及测试步骤定义

---
+++ src/main/java/com/junwen/bdd/Calculator.java
@@ -0,0 +1,7 @@
+package com.junwen.bdd;
+
+public class Calculator {
+    public Integer add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/junwen/bdd/CalculatorStepDefinitions.java
@@ -0,0 +1,27 @@
+package com.junwen.bdd;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorStepDefinitions {
+    Calculator calculator;
+    Integer result;
+
+    @Given("I have a calculator")
+    public void i_have_a_calculator() {
+        calculator = new Calculator();
+    }
+
+    @When("I add {int} and {int}")
+    public void i_add_and(Integer int1, Integer int2) {
+        result = calculator.add(int1, int2);
+    }
+
+    @Then("I should get {int}")
+    public void i_should_get(Integer int1) {
+        assertThat(result).isEqualTo(int1);
+    }
+}
